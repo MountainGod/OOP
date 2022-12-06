6f4a2feb336fa128899c4050974180e14d6bb442
Harel Karni <MountainGodRays@gmail.com>
2022-12-06 13:52:35 +0200

    idk not much happened

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1;
+﻿namespace OOP1;
 
 internal class Program
 {
---
+++ OOP2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP2;
+
+internal class Program
+{
+    private bool m(int x)
+    {
+        return x == 23;
+    }
+    
+    
+    public static void Main(string[] args)
+    {
+        int hi = int.Parse(Console.ReadLine());
+        bool h = true;
+        Console.WriteLine(hi);
+
+        if (hi == 23)
+            h = true;
+        else
+            h = false;
+        
+        
+    }
+}
